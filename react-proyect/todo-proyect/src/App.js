42661df514d9c7b4cd158daa11f66df5877f4f97
Esteban Javier <estebandiaz199cuatro@gmail.com>
2021-09-09 18:06:11 -0300

    showing vs.code github 1

--- react-proyect/todo-proyect/src/App.js
+++ react-proyect/todo-proyect/src/App.js
@@ -4,7 +4,7 @@ import  "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import TodoItem from "./components/TodoItem";
-
+// showing vs.code github 1
 class App extends Component {
 
   render() {
